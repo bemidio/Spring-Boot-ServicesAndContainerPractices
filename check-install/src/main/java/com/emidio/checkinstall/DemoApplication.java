a2e29e05627e3e1eec73e4722c91ce37e48f5c86
Fernando Emidio <fernando.bemidio@gmail.com>
2019-11-23 19:40:21 -0300

    removendo arquivos desnecessários

--- check-install/src/main/java/com/emidio/checkinstall/DemoApplication.java
+++ check-install/src/main/java/com/emidio/checkinstall/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.emidio.checkinstall;
 
-import com.emidio.checkinstall.entididade.CategoriaProdutoEnum;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,17 +11,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	 @Bean
-	 public static CommandLineRunner commandLineRunner() {
-
-		return args -> {
-
-			CategoriaProdutoEnum aaa =  CategoriaProdutoEnum.CAMA_MESA_BANHO;
-
-			System.out.println("o valor é " + aaa.name());
-		};
 	}
 
 }
